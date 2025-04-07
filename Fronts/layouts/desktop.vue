2a3bf09faec5df63b9a2f0d80fc0ad573f3195dd
Riuchek <riuchek@gmail.com>
2025-04-07 18:50:12 -0300

    feat: modules

---
+++ Fronts/layouts/desktop.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <HeaderDesktop />
+      <h1>
+        desktop
+      </h1>
+    <NuxtPage />
+    <FooterDesktop />
+  </div>
+</template>
